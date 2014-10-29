375e80e5dc11b2f78e899395985050e9214ede81
oitsmalab <oitsmalab@gmail.com>
2014-10-29 18:16:35 +0900

    みてわかるUnity超入門の4章のアニメーションを基にしたプロジェクト。

---
+++ New Unity Project 38/Assets/CameraAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AnimationClip clip = new AnimationClip ();
+		AnimationCurve curve = AnimationCurve.Linear (0f, 3f, 3f, 3f);
+		Keyframe key = new Keyframe (1.5f, 7f);
+		curve.AddKey (key);
+		clip.SetCurve ("", typeof(Transform), "localPosition.z", curve);
+		clip.wrapMode = WrapMode.Default;
+		animation.AddClip (clip, "clip1");
+		animation.Play ("clip1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			animation.Stop ();
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			animation.Play ("clip1");
+		}
+	}
+}
---
+++ New Unity Project 38/Assets/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	
+	Animation animation;
+
+
+	// ゲーム初期化処理
+	void Start () {
+		animation = GetComponent<Animation>();
+	}
+	
+	// frameごとに呼び出される
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			animation.Play ("camera_move_right");
+				}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			animation.Play ("camera_move_left");
+		}
+
+		}
+	}
