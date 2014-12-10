f8a21b4e97afb07f002df017a5607f5f3d73fcb4
oitsmalab <oitsmalab@gmail.com>
2014-12-10 16:07:37 +0900

    slideの枚数を4から7に変更 select2.cs -> MoveCamera.csに差し替え

---
+++ New Unity Project 17/Assets/Kawai/script/MoveCamera.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+	
+	public AnimationCurve[] animationCurvePattern = new AnimationCurve[7];//予め設定する値（アニメーションのパターン）0-4で指定
+	public  GameObject[] imageArrays = new GameObject[2];//移動先オブジェクト
+	public int[] moveAnimationPatterns = new int[7] ;//アニメーションのパターンの順番
+	int countObject = -1;//現在のオブジェクトの位置 始めに０番目の地点へ右キーで移動
+	
+	struct Curve{
+		public float height;//基準の高さ
+		public float[] keyframeValues;//相対高度
+	}
+	
+	Curve[] curveArray = new Curve[7];
+	
+	public AnimationCurve[] animationCurvePatternZoom = new AnimationCurve[1];
+	Curve[] curveZoom =new Curve[1];
+	
+	
+	void Start () {
+		GameObject.Find ("GUI Text").guiText.text = "moveAnimation3 sample";
+		
+		GetRelativeHeightsFromAnimationCurvePattern (animationCurvePattern,curveArray);
+		
+		if (animationCurvePatternZoom.Length >= 1) {
+			if (animationCurvePatternZoom [0].length >= 2) {
+				GetRelativeHeightsFromAnimationCurvePattern (animationCurvePatternZoom, curveZoom);
+				
+			}
+		}
+		Debug.Log (curveArray [0].height+ " firstHeight");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		RightArrowButton();
+		LeftArrowButton();
+	
+	}
+	
+	
+	void GetRelativeHeightsFromAnimationCurvePattern(AnimationCurve[] animationCurvePattern, Curve[] curveArray){
+		//Debug.Log ("animationCurvePattern.Length=" + animationCurvePattern.Length);
+		for (int a=0; a<animationCurvePattern.Length; a++) {
+			curveArray [a].keyframeValues = new float[animationCurvePattern [a].length];
+		}//長さ確定　インスタンス化
+		
+		for (int k=0; k<animationCurvePattern.Length; k++) {
+			curveArray [k].height = 
+				animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[animationCurvePattern [k].length- 1].time) 
+					- animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[0].time);
+			///Debug.Log("curveArray["+k+"].height="+curveArray[k].height);
+			for (int i=1; i<animationCurvePattern[k].length-1; i++) {
+				/*Debug.Log("animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[i].time)_"+animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[i].time));
+				Debug.Log("animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[0].time)_"+animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[0].time));*/
+				curveArray [k].keyframeValues [i] = animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[i].time)
+					- animationCurvePattern [k].Evaluate (animationCurvePattern [k].keys[0].time);
+			}//相対高度へ変換
+		}//配列curveの各要素の値確定
+	}
+	
+	
+	void moveAnimation3 (Vector3 targetPosition ,int animationNumber){
+		AnimationClip clip3 = new AnimationClip ();
+		Debug.Log("X");
+		AnimationCurve curveX =
+			GetAdjustedAnimationCurve(targetPosition.x,gameObject.transform.position.x,animationCurvePattern,animationNumber,curveArray);
+		
+		Debug.Log("Y");
+		AnimationCurve curveY =
+			GetAdjustedAnimationCurve(targetPosition.y,gameObject.transform.position.y,animationCurvePattern,animationNumber,curveArray);
+		
+		Debug.Log ("Z");
+		AnimationCurve curveZ = animationCurvePatternZoom [0];
+		//GetAdjustedAnimationCurve (targetPosition.z - 2, gameObject.transform.position.z, animationCurvePatternZoom, 0, curveZoom);
+		clip3.SetCurve ("", typeof(Transform), "localPosition.z", curveZ);
+		
+		Debug.Log("Curvechanged");
+		
+		clip3.SetCurve ("", typeof(Transform), "localPosition.x", curveX);
+		clip3.SetCurve ("", typeof(Transform), "localPosition.y", curveY);
+		animation.AddClip (clip3, "moveclip3");
+		animation.Play ("moveclip3");
+		
+	}
+	
+	AnimationCurve GetAdjustedAnimationCurve(float targetPosition,float transformPosition,AnimationCurve[] animationCurvePattern ,int animationNumber ,Curve[] curveArray){
+		
+		AnimationCurve curve = new AnimationCurve ();
+		curve.keys = animationCurvePattern[animationNumber].keys;
+		
+		float distanceToGo = targetPosition - transformPosition;
+		float distanceToGoAdjusted = distanceToGo / curveArray [animationNumber].height; //元の幅と変更後の幅の比率
+		
+		Debug.Log ("distanceTOGo_" + distanceToGo);
+		
+		curve.MoveKey (0,new Keyframe (curve.keys[0].time, transformPosition));//配列0ばんめのキーフレーム変更
+		curve.MoveKey (curve.length - 1, new Keyframe (curve.keys[curve.length - 1].time, targetPosition) );//終端キーフレーム変更
+		
+		for (int i=1; i<animationCurvePattern[animationNumber].length-1; i++) {
+			curve.MoveKey (i, new Keyframe (curve.keys[i].time, (curve.Evaluate (curve.keys[0].time) + curveArray [animationNumber].keyframeValues [i] * distanceToGoAdjusted)  )  );
+			Debug.Log("distanceToGoAdjusted_"+distanceToGoAdjusted);
+			Debug.Log("curveArray_"+curveArray[animationNumber].keyframeValues[i]);
+			Debug.Log(curve.keys[i].time + "time  " + "number "+i);
+			Debug.Log(animationCurvePattern[animationNumber].Evaluate(animationCurvePattern[animationNumber].keys[i].time) +" firstkey"+i +" anime"+animationNumber);
+			Debug.Log(curve.Evaluate (curve.keys[i].time) + " changedkey"+i);
+			
+		}//中間のキーフレーム置き換え
+		
+		Debug.Log (transformPosition+" c.P " +targetPosition+" t.P");
+		Debug.Log(distanceToGoAdjusted + " changeheight");
+		
+		return curve;
+	}
+
+	public void	RightArrowButton(){	
+		if(countObject < imageArrays.Length-1){
+			countObject ++;
+			moveAnimation3(imageArrays[countObject].transform.position,moveAnimationPatterns[countObject]);
+			Debug.Log (1 +" cnt_"+countObject);
+		}
+	}
+	
+	public void	LeftArrowButton(){
+		if(countObject > 0){
+			countObject --;
+			moveAnimation3(imageArrays[countObject].transform.position, moveAnimationPatterns[countObject]);
+			Debug.Log (2+" cnt_"+countObject);
+		}
+	}
+	
+	
+}
---
+++ New Unity Project 17/Assets/Kawai/script/Moveimage.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveimage : MonoBehaviour {
+	public AnimationCurve curve;//ここで３軸すべてのカーブの形を決める
+	float M0;
+	float[] Eva;
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("GUI Text").guiText.text = "moveAnimation sample";
+		Eva = new float[curve.length];
+		M0 = curve.Evaluate(curve.length - 1) - curve.Evaluate(0);
+		for(int i=1; i<curve.length-1; i++){
+			Eva[i] = curve.Evaluate (i) - curve.Evaluate (0);
+		}//相対高度へ変換
+	}
+
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetMouseButtonUp (0)) {
+						Debug.Log ("左クリック");
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+			if(Physics.Raycast (ray,out hit) && gameObject != hit.collider.gameObject){
+				moveAnimation2(hit.collider.gameObject.transform.position);
+				Debug.Log ("選択移動");
+			}
+				}
+	}
+
+	void moveAnimation2 (Vector3 p2){
+		AnimationCurve curvex = new AnimationCurve();
+		curvex.keys = curve.keys;
+
+		float X, X1;
+		//x軸
+		X1 = p2.x - gameObject.gameObject.transform.position.x;
+		X = X1 / M0; //元の幅と変更後の幅の比率
+		Keyframe x1 = new Keyframe (0,gameObject.transform.position.x);//配列0ばんめのキーフレーム変更
+		Keyframe x2 = new Keyframe (curve.length-1,p2.x);//終端キーフレーム変更
+
+		curvex.MoveKey (0,x1);
+		curvex.MoveKey(curvex.length-1, x2);//両端キーフレーム置き換え
+
+	for (int i=1; i<curve.length-1; i++) {
+			Debug.Log (i);
+		curvex.MoveKey (i,new Keyframe(i,curvex.Evaluate(0) + Eva[i]* X)  );
+		}//中間のキーフレーム置き換え
+
+
+		//y
+	 AnimationCurve curvey = new AnimationCurve();
+	 curvey.keys = curve.keys;
+		float Y, Y1;
+		Y1 = p2.y - gameObject.gameObject.transform.position.y;
+		Y = Y1 / M0; //元の幅と変更後の幅の比率
+		Keyframe y1 = new Keyframe (0,gameObject.transform.position.y);//配列0ばんめのキーフレーム変更
+		Keyframe y2 = new Keyframe (curve.length-1,p2.y);//終端キーフレーム変更
+		
+		curvey.MoveKey (0,y1);
+		curvey.MoveKey(curvey.length-1, y2);//両端キーフレーム置き換え
+		
+		for (int i=1; i<curve.length-1; i++) {
+			Debug.Log (i);
+			curvey.MoveKey (i,new Keyframe(i,curvey.Evaluate(0) +  Eva[i] * Y)  );
+		}//中間のキーフレーム置き換え
+
+
+		//z
+		AnimationCurve curvez = new AnimationCurve();
+		curvez.keys = curve.keys;
+		float Z, Z1;
+		Z1 = p2.z - gameObject.gameObject.transform.position.z;
+		Z = Z1 / M0; //元の幅と変更後の幅の比率
+		Keyframe z1 = new Keyframe (0,gameObject.transform.position.z);//配列0ばんめのキーフレーム変更
+		Keyframe z2 = new Keyframe (curve.length-1,p2.z);//終端キーフレーム変更
+		
+		curvez.MoveKey (0,z1);
+		curvez.MoveKey(curvez.length-1, z2);//両端キーフレーム置き換え
+		
+		for (int i=1; i<curve.length-1; i++) {
+			Debug.Log (i);
+			curvez.MoveKey (i,new Keyframe(i,curvez.Evaluate(0) +  Eva[i] * Z)  );
+		}//中間のキーフレーム置き換え
+
+
+		AnimationClip clip2 = new AnimationClip ();
+		clip2.SetCurve ("", typeof(Transform), "localPosition.x", curvex);
+		clip2.SetCurve ("", typeof(Transform), "localPosition.y", curvey);
+		clip2.SetCurve ("", typeof(Transform), "localPosition.z", curvez);
+		animation.AddClip (clip2, "moveclip2");
+		animation.Play ("moveclip2");
+	
+	}
+	
+	}
+
