9983125bed24657a75945bb59e6ad8e2130661cb
oitsmalab <oitsmalab@gmail.com>
2015-01-15 14:06:39 +0900

    ONclickでカメラ移動

---
+++ New Unity Project 17/Assets/cameraback.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraback : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	public void camera_back(){
+			transform.position = new Vector3 (30f, 5f, -45f);
+	}
+}
---
+++ New Unity Project 17/Assets/target.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class target : MonoBehaviour {
+	public int cnt=2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void slide_click() {
+		transform.position = new Vector3 (0f, 0f, -2.844865f);
+		cnt++;
+		return;
+	}
+
+	public void slidesound(){
+			audio.Play();
+		return;
+		}
+
+	public void soundend(){
+		audio.Stop ();
+		}
+
+
+}
